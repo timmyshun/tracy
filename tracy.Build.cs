33f4777664179bd2e2c972d08115f9fb67a0f4ca
timmyshun <254093960@qq.com>
2022-07-18 21:17:30 +0800

    [ADD] tracy.Build.cs for UE. and fix compile.

---
+++ tracy.Build.cs
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.IO;
+
+public class tracy : ModuleRules
+{
+	public tracy(ReadOnlyTargetRules Target) : base(Target)
+	{
+		Type = ModuleType.External;
+
+		string TracyPath = Target.UEThirdPartySourceDirectory + "tracy/";
+
+		PublicSystemIncludePaths.Add(TracyPath);
+	}
+}
